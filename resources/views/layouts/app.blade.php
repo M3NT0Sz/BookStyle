8f26672978bdd086e5f896f8b5a6de8063620e1c
PauloDiney <paulo19martin@gmail.com>
2025-03-12 17:00:31 -0300

    Estilizando Index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="{{ Vite::asset('resources/img/favicon.png') }}" type="image/x-icon">
     <title>Document</title>
     @vite('resources/css/app.css')
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,101 @@
 @extends('layouts.app')
+
+@section('content')
+    <header class="header-container">
+        <nav class="nav-container">
+            <img class="logo" src="{{ Vite::asset('resources/img/logo1.png') }}" alt="">
+            <ul class="nav-links">
+                <li>
+                    <a href="#Home">Home</a>
+                </li>
+                <li>
+                    <a href="#Home">Home</a>
+                </li>
+                <li>
+                    <a href="#Home">Home</a>
+                </li>
+                <li>
+                    <a href="#Home">Home</a>
+                </li>
+
+
+            </ul>
+            <button class="button-login">
+                Entrar
+            </button>
+
+        </nav>
+
+        <div class="banner-container">
+            <h1>BookStyle</h1>
+            <p>Encontre suas Histórias</p>
+            <section class="search-container">
+                <input type="text" placeholder="Pesquise por um livro">
+
+            </section>
+            <section class="books-down">
+                <span class="book1"></span>
+                <span class="book2"></span>
+                <span class="book3"></span>
+                <span class="book4"></span>
+                <span class="book1"></span>
+                <span class="book2"></span>
+                <span class="book3"></span>
+                <span class="book4"></span>
+                <span class="book1"></span>
+                <span class="book2"></span>
+                <span class="book3"></span>
+                <span class="book4"></span>
+                <span class="book1"></span>
+                <span class="book2"></span>
+                <span class="book3"></span>
+                <span class="book4"></span>
+                <span class="book1"></span>
+                <span class="book2"></span>
+                <span class="book3"></span>
+                <span class="book4"></span>
+                <span class="book1"></span>
+                <span class="book2"></span>
+                <span class="book3"></span>
+                <span class="book4"></span>
+                <span class="book1"></span>
+                <span class="book2"></span>
+                <span class="book3"></span>
+                <span class="book4"></span>
+            </section>
+        </div>
+    </header>
+    <main>
+        <section class="books-container">
+            <h2>Livros</h2>
+            <section class="categori-container">
+                <h1 class="h1">Aventura</h1>
+                <section class="books">
+                    <div class="book">
+                        <img src="{{ Vite::asset('resources/img/book1.jpg') }}"alt="">
+                        <h1>Titulo</h1>
+                        <p>Autor</p>
+                    </div>
+                    <div class="book">
+                        <img src="{{ Vite::asset('resources/img/book1.jpg') }}" alt="">
+                        <h1>Titulo</h1>
+                        <p>Autor</p>
+                    </div>
+                    <div class="book">
+                        <img src="{{ Vite::asset('resources/img/book1.jpg') }}" alt="">
+                        <h1>Titulo</h1>
+                        <p>Autor</p>
+                    </div>
+                    <div class="book">
+                        <img src="{{ Vite::asset('resources/img/book1.jpg') }}" alt="">
+                        <h1>Titulo</h1>
+                        <p>Autor</p>
+                    </div>
+
+                    <button class="button-left"><</button>
+                    <button class="button-right">></button>
+            </section>
+
+        </section>
+    </main>
+@endsection
